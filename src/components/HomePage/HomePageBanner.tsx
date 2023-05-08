75c2c8de2fd449e0140a354e264c8e86c6cf589f
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-05-08 14:35:17 -0700

    Add HomePageBanner component

---
+++ src/components/HomePage/HomePageBanner.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./HomePageBanner.css"
+
+//import images
+import redBegoniaImage from "./HomePage_Images/red-begonia-icon.png"
+import bottieImage from "./HomePage_Images/bottie-icon.png"
+
+
+interface HomePageBannerProps {
+
+}
+
+export const HomePageBanner: React.FC<HomePageBannerProps> = () => {
+    return (
+      <div className='home-page-banner'>
+        <div className='home-page-banner-content'>
+          <div className='red-begonia-icon'>
+            <img className="red-begonia-img" src={redBegoniaImage} alt="Red Wing Begonia"></img>
+          </div>
+          <div className='home-banner-center'>
+            <h1> Hello, I am Bottie. I will help you to find the perfect leafy gift. </h1>
+            <p>In order to begin, tell me more about that person you want to offer a special gift.</p>
+            <button className="main_button">Let's get started!</button>
+          </div>
+          <div className='bottie-icon'>
+            <img className="bottie-img" src={bottieImage} alt="Bottie"></img>
+          </div>
+        </div>
+      </div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 
 //import styling
 import "./Home.css"
+
+//import components
+import { HomePageBanner } from "../components/HomePage/HomePageBanner";
 
 interface HomeProps {}
 
@@ -11,9 +12,8 @@ export const Home: React.FC<HomeProps> = () => {
   return (
     <main>
       <div className="spacer-tag home" />
-      <section className="pages">
-        <h1>This is my React app!</h1>
-        <Link to="/products">All plants</Link>
+      <section >
+        <HomePageBanner />
         <p>
           Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
           De carne lumbering animata corpora quaeritis. Summus brains sit​​,
