d6c20b1ffce92ce8e9a2094d3f8452c1f9a01f44
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-05-09 12:21:47 -0700

    Add HowItWorks component and HomeBanner sixing fix

--- src/components/HomePage/HowItWorks.tsx
+++ src/components/HomePage/HowItWorks.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import "./HowItWorks.css"
+import React from "react";
+import "./HowItWorks.css";
 
 //import photos
+import num1 from "./HomePage_Images/num1.png";
+import num2 from "./HomePage_Images/num2.png";
+import num3 from "./HomePage_Images/num3.png";
 
-interface HowItWorksProps {
 
-}
+interface HowItWorksProps {}
 
 export const HowItWorks: React.FC<HowItWorksProps> = () => {
-    return (
-      // <div className='block'>
-  <div className='how-it-works-block'>
-        {/* <img className="flower-background" src={backgroundImage} alt="Flower Background"></img> */}
-          <div>
-            <h3>How it works</h3>
-          </div>
+  return (
+    <div className="how-it-works-block">
+      <div className="how-it-works-title">
+        <h3>How It Works</h3>
+      </div>
+      <div className="step-blocks-section">
+        <div className="step-block">
+          <img className="num1" src={num1} alt="Number One"></img>
+          <p>Tell Bottie about you and the person you want to offer a present.</p>
         </div>
-      // </div>
-    );
-}
+        <div className="step-block">
+          <img className="num2" src={num2} alt="Number Two"></img>
+          <p>Choose among the plants recommended for your gift.</p>
+        </div>
+        <div className="step-block">
+          <img className="num3" src={num3} alt="Number Three"></img>
+          <p>Get a personalized card for your special someone.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
