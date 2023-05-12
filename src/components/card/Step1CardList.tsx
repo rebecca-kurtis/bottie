fa1b9906112b55c8fe37895c33ecbbbdb3579288
amchampoux <amchampoux@mirego.com>
2023-05-11 21:58:14 -0400

    add datas to flow

--- src/components/card/Step1CardList.tsx
+++ src/components/card/Step1CardList.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import products from "../data/products.json";
+// import products from "../data/products.json";
 import { Step1Card } from './Step1Card';
 
 
-interface Step1CardListProps {}
+interface Step1CardListProps {
+  products?:any[];
+}
 
-export const Step1CardList: React.FC<Step1CardListProps> = () => {
+export const Step1CardList: React.FC<Step1CardListProps> = ({products}) => {
 
-
-  const mappedProduct = products.map((product) => {
+  const mappedProduct = products?.map((product) => {
+    const price = (product.price_in_cents/100).toString();
     return <Step1Card
       key={product.id} 
       name={product.name} 
       description={product.description} 
-      price={product.price_in_cents}
-      imageSrc={product.image_draw}
+      price={price}
+      imageSrc={product.drawing_url}
     /> 
   });
   
