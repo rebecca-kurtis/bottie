03941a43c4d98d01917987ba67d3f89fd0127650
amchampoux <amchampoux@mirego.com>
2023-05-17 11:28:41 -0400

    add empty cart view

--- src/components/_partials/_MiniCart.tsx
+++ src/components/_partials/_MiniCart.tsx
@@ -6,6 +6,7 @@ import { MiniCartCard } from "./_MiniCartCard";
 import { SecondaryButton } from "../SecondaryButton";
 import { MainButton } from "../MainButton";
 import { useNavigate } from "react-router-dom";
+import potImage from "./images/pot.png";
 
 
 interface _MiniCartProps {
@@ -17,36 +18,39 @@ interface _MiniCartProps {
 export const MiniCart: React.FC<_MiniCartProps> = ({products, closeSide}) => {
 
 
-const [cart, setCart] = useState(1)  
+const [cart, setCart] = useState(null)  
 
 const product = products[0];
 
 let navigate = useNavigate();
 
-const routeChange = () => {
+const cartRouteChange = () => {
 let path = `/card`;
 closeSide();
 navigate(path);
 window.scrollTo(0, 0);
 };
 
+const plantRouteChange = () => {
+  let path = `/products`;
+  closeSide();
+  navigate(path);
+  window.scrollTo(0, 0);
+  };
 
   return (
     <div className="user">
        <div className="canva-body">
           <h2>Cart</h2>
           <br></br>
-          <br></br>
-
           {cart === null &&
             <div>
-              <p className="canva_body">You can track your orders and access Bottie's promotions with your personal account.</p>
+              <img src={potImage} alt="Empty pot" className="user-image"/> 
               <br></br>
-              <h4>Orders</h4>
               <br></br>
-              <div className="orders-container">
-                <h5>Cart is null.</h5>
-              </div>
+              <p className="canva_body">You have nothing in your cart. Why not start by looking into my currated plant selection?</p>
+              <br></br>
+              <MainButton type="submit" name="View all products" onChange={plantRouteChange}/>
             </div>
           }
           {cart !== null &&
@@ -55,7 +59,7 @@ window.scrollTo(0, 0);
               <MiniCartCard products={products} />
               <br></br>
               <br></br>
-              <MainButton type="submit" name="Go to my cart" onChange={routeChange}/>
+              <MainButton type="submit" name="Go to my cart" onChange={cartRouteChange}/>
             </div>
           }
       </div> 
