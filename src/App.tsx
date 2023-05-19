49f8428df687a5fcdda657860677b42cef52dee7
amchampoux <amchampoux@mirego.com>
2023-05-18 23:25:43 -0400

    add cart info to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,14 @@ import { Profile } from './pages/Profile';
 import { CartConfirm } from './pages/CartConfirm';
 import { Cart } from './pages/Cart';
 
+//import hooks
+import { useLocalStorage } from './hooks/useLocalStorage';
+
 function App() {
 
   // Product API call
 
   const [products, setProducts] = useState([] as any[]);
-  const [user, setUser] = useState(null);
-
-  const [cart, setCart] = useState([] as any[]);
-  const [total, setTotal] = useState(0);
-  const [totalandTax, setTotalAndTax] = useState(0);
-  const [tax, setTax] = useState(0);
-
   const productsRoute = process.env.REACT_APP_SERVER + ":" + process.env.REACT_APP_SERVER_PORT + "/products"
   
   useEffect(() => {
@@ -38,19 +34,22 @@ function App() {
 
   // Set current user state
 
-  function getCurrentUser() {
-    const userStr = localStorage.getItem("user");
-    console.log(userStr)
-    if (userStr) return JSON.parse(userStr);
+  // const [user, setUser] = useState(null);
+  const [user, setUser] = useLocalStorage("user", null);
 
-    return null;
-  }
+  // function getCurrentUser() {
+  //   const userStr = localStorage.getItem("user");
+  //   console.log(userStr)
+  //   if (userStr) return JSON.parse(userStr);
 
-  useEffect(() => {
-    const currentUser = getCurrentUser()
-    setUser(currentUser);
+  //   return null;
+  // }
 
-  }, []);
+  // useEffect(() => {
+  //   const currentUser = getCurrentUser()
+  //   setUser(currentUser);
+
+  // }, []);
 
   function updateUserStorage(currentUser: React.SetStateAction<null>) {
     setUser(currentUser);
@@ -72,16 +71,54 @@ function App() {
     return totalReduce;
   }
 
+  // Set current cart state
+
+  // const [cart, setCart] = useState([] as any[]);
+  // const [total, setTotal] = useState(0);
+  // const [totalandTax, setTotalAndTax] = useState(0);
+  // const [tax, setTax] = useState(0);
+
+  const [cart, setCart] = useLocalStorage("cart", [] as any[]);
+  const [total, setTotal] = useLocalStorage("total", 0);
+  const [totalandTax, setTotalAndTax] = useLocalStorage("totalandTax", 0);
+  const [tax, setTax] = useLocalStorage("tax", 0);
+
+  // function getUserOrderInfo(dataCall: any) {
+  //       setCart(dataCall);
+  //       const totalSum = dataCall.map((cartItem:any) => cartItem.product_price);
+  //       const total = Math.round(sumArray(totalSum)) / 100;
+  //       setTotal(Math.round(total/100))
+  //       const tax = Math.round((total * .05) * 100) / 100;
+  //       setTax(tax);
+  //       const totalandTax = Math.round((total + tax) * 100) / 100;
+  //       setTotalAndTax(totalandTax)
+  // }
+
   function getUserOrderInfo(dataCall: any) {
-        setCart(dataCall);
-        const totalSum = dataCall.map((cartItem:any) => cartItem.product_price);
-        const total = Math.round(sumArray(totalSum)) / 100;
-        setTotal(Math.round(total/100))
-        const tax = Math.round((total * .05) * 100) / 100;
-        setTax(tax);
-        const totalandTax = Math.round((total + tax) * 100) / 100;
-        setTotalAndTax(totalandTax)
-    }
+    setCart(dataCall);
+    localStorage.clear();
+    localStorage.setItem("cart", JSON.stringify(dataCall));
+
+    const totalSum = dataCall.map((cartItem:any) => cartItem.product_price);
+    const total = Math.round(sumArray(totalSum)) / 100;
+    setTotal(Math.round(total/100))
+
+    const tax = Math.round((total * .05) * 100) / 100;
+    setTax(tax);
+    localStorage.setItem("tax", JSON.stringify(tax));
+
+    const totalandTax = Math.round((total + tax) * 100) / 100;
+    setTotalAndTax(totalandTax)
+    localStorage.setItem("totalandTax", JSON.stringify(totalandTax));
+}
+
+  // Remove current user state
+  function clearCartStorage() {
+    localStorage.removeItem("cart");
+    localStorage.removeItem("tax");
+    localStorage.removeItem("totalandTax");
+    setCart([] as any[]);
+  }
 
   return (
     <>
@@ -94,7 +131,7 @@ function App() {
       <Route path="/profile" element={<Profile />} />
       <Route path="/card" element={<CardIndex products={products} user={user}/>} />
       <Route path="/confirmation" element={<CartConfirm/>} />
-      <Route path="/cart" element = {<Cart user={user} cart={cart} tax={tax} totalandTax={totalandTax} />} />
+      <Route path="/cart" element = {<Cart user={user} cart={cart} tax={tax} totalandTax={totalandTax} clearCartStorage={clearCartStorage} />} />
       {/* <Route path="/card/configure" element={<CardConfigure />} /> */}
     </Routes>
   </BrowserRouter>
